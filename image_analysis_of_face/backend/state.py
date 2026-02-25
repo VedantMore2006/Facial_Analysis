d01534c39a3f6f5c6626e1ceb1aecb77d21fa11b
VedantMore2006 <vedantmoremain@gmail.com>
2026-02-25 18:24:17 +0530

    doing everything again

--- image_analysis_of_face/backend/state.py
+++
@@ -1,3 +0,0 @@
-# backend/state.py
-pipeline_running = False
-pipeline_thread = None
--- image_analysis_of_face/backend/test.py
+++
@@ -1,7 +0,0 @@
-# backend/test_worker.py
-import time
-import camera_worker
-
-camera_worker.start_pipeline()
-time.sleep(10)
-camera_worker.stop_pipeline()
